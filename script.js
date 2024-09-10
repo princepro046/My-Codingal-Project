f62ef30bda79af900a0437e21c069193407ffc46
princepro046 <princepro046@gmail.com>
2024-09-10 20:28:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function divide(num) {
+           return num / 10;
+}
+var numbers = [28,6,19,95]
+document.write(answer);
+
+function java_math() {
+           var x = 10;
+           var y = 20;
+           var a = eval("x + y") + "<br>";
+
+         var res = a; //concatenates the string
+         document.getElementById("result").innerHTML = res
+
+         var points = [40,100,1,5,25,10];
+         document.getElementById("result").innerHTML = points;
+         function ascending() {
+            points.sort(function(a, b){return a-b});
+            document.getElementById("result").innerHTML = points;
+         }
